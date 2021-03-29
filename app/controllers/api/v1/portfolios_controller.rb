19f3ec2b2751b4e11ec651bef61256360521f09b
CoderJay06 <jaycru06@yahoo.com>
2021-03-29 04:05:31 -0400

    Add api, v1 folders, namespace controllers & routes

---
+++ app/controllers/api/v1/portfolios_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::PortfoliosController < ApplicationController
+   def index 
+      # byebug
+   end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  resources :users
+   # api/v1/
+   namespace :api do 
+      namespace :v1 do 
+         resources :portfolios
+         resources :users
+      end 
+   end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
