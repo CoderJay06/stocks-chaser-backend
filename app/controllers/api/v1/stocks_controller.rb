13d47ebadee2fb43f61b0f53d2c61ac91250ee71
CoderJay06 <jaycru06@yahoo.com>
2021-03-30 12:13:10 -0400

    Setup stocks controller

--- app/controllers/api/v1/stocks_controller.rb
+++ app/controllers/api/v1/stocks_controller.rb
@@ -1,5 +1,49 @@
 class Api::V1::StocksController < ApplicationController
+   before_action :find_stock, only: [:show, :update, :destory]
+
    def index 
       # byebug
+      stocks = Stock.all 
+
+      render json: stocks 
    end 
+
+   def create 
+      stock = Stock.create(stock_params)
+
+      if stock.save 
+         render json: stock
+      else  
+         render json: { error: 'Error adding this stock' }
+      end 
+   end 
+
+   def show 
+      render json: @stock 
+   end 
+
+   def update 
+      @stock.update(stock_params)
+
+      if @stock.save  
+         render json: @stock
+      else  
+         render json: { error: 'Error updating this stock' }
+      end 
+   end 
+
+   def destroy 
+      @stock.destroy 
+   end 
+
+   private 
+
+   def stock_params 
+      params.require(:stock).permit(:ticker, :name, :price_per_share)
+   end 
+
+   def find_stock
+      @stock = Stock.find(params[:id]) 
+   end 
+
 end
