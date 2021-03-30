f49fb471138fef404000b6b608194bb3eb291ac3
CoderJay06 <jaycru06@yahoo.com>
2021-03-29 20:48:05 -0400

    Generate Stock model and drop db

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,4 @@
 class Portfolio < ApplicationRecord
    belongs_to :user 
+   has_and_belongs_to_many :stocks
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :stocks
    # api/v1/
    namespace :api do 
       namespace :v1 do 
