95b913ee6fa7bcf2eea4a0947f3e92bffeb43095
CoderJay06 <jaycru06@yahoo.com>
2021-04-18 20:42:09 -0400

    Modify portfolio stocks component to implement create action

--- app/controllers/api/v1/portfolio_stocks_controller.rb
+++ app/controllers/api/v1/portfolio_stocks_controller.rb
@@ -3,9 +3,18 @@ class Api::V1::PortfolioStocksController < ApplicationController
       # need to add new stock to users portfolio
       # byebug
       # TODO: need to add/save new stocks for users portfolio
-      portfolio_stocks = PortfolioStock.create()
+      stock = Stock.create(
+         ticker: params[:portfolio_stock][:stock]["ticker"],
+         name: params[:portfolio_stock][:stock]["name"],
+         price_per_share: params[:portfolio_stock][:stock]["price_per_share"]
+      )
 
-      render json: portfolio_stocks, status: :accepted
+      portfolio_stock = PortfolioStock.create({
+         portfolio_id: params[:portfolio_stock][:portfolio][:id],
+         stock_id: stock.id
+      });
+      # byebug
+      render json: portfolio_stock, include: [:stock], status: :accepted
    end 
 
    def update 
@@ -14,7 +23,7 @@ class Api::V1::PortfolioStocksController < ApplicationController
    def destroy
    end 
 
-   private 
+   # private 
    # def portfolio_stocks_params 
    #    params.require(:portfolio_stocks).permit(:portfolio_id, :stock_id)
    # end 
