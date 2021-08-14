482e803496483dba7853bae3dbf9bb4ecec328b3
CoderJay06 <jaycru06@yahoo.com>
2021-08-14 03:45:01 -0400

    Reset database and modify seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_184542) do
+ActiveRecord::Schema.define(version: 2021_03_30_132131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2021_03_30_184542) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "username"
-    t.string "password_digest"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
